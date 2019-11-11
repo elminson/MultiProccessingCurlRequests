8cb71f37327e94e7cf22ecee37d74d277c1b701c
Elminson De Oleo Baez <elminson@refersion.com>
2019-11-11 13:16:32 -0500

    Fixing when send post and json data + format file

--- src/MultiProccessingCurlRequests.php
+++ src/MultiProccessingCurlRequests.php
@@ -14,140 +14,156 @@ namespace Elminson\MultiProccessingCurlRequests;
  */
 class MultiProccessingCurlRequests
 {
-    private $isJson = false;
 
-    private $isDebug = false;
+	private $isJson = false;
 
-    private $isPost = true;
+	private $isDebug = false;
 
-    /**
-     * PHPProjectGen constructor.
-     */
-    public function __construct()
-    {
-    }
+	private $isPost = true;
 
-    /**
-     * @param $data
-     * @param array $options
-     * @return array
-     */
-    function multiRequest($data, $options = array())
-    {
+	/**
+	 * PHPProjectGen constructor.
+	 */
+	public function __construct()
+	{
+	}
 
-        // array of curl handles
-        $curly = array();
-        // data to be returned
-        $result = array();
+	/**
+	 * @param       $data
+	 * @param array $options
+	 *
+	 * @return array
+	 */
+	function multiRequest($data, $options = array())
+	{
 
-        // multi handle
-        $mh = curl_multi_init();
+		// array of curl handles
+		$curly = array();
+		// data to be returned
+		$result = array();
 
-        // loop through $data and create curl handles
-        // then add them to the multi-handle
-        foreach ($data as $id => $d) {
-            $curly[$id] = curl_init();
+		// multi handle
+		$mh = curl_multi_init();
 
-            $url = (is_array($d) && !empty($d['url'])) ? $d['url'] : $d;
-            curl_setopt($curly[$id], CURLOPT_URL, $url);
-            curl_setopt($curly[$id], CURLOPT_HEADER, 0);
-            curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, true);
-            if ($this->isDebug) {
-                curl_setopt($curly[$id], CURLOPT_VERBOSE, true);
-            }
+		// loop through $data and create curl handles
+		// then add them to the multi-handle
+		foreach ($data as $id => $d) {
+			$curly[$id] = curl_init();
 
-            if ($this->isPost) {
-                curl_setopt($curly[$id], CURLOPT_POST, true);
-            }
+			$url = (is_array($d) && !empty($d['url'])) ? $d['url'] : $d;
+			curl_setopt($curly[$id], CURLOPT_URL, $url);
+			curl_setopt($curly[$id], CURLOPT_HEADER, 0);
+			curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, true);
+			if ($this->isDebug) {
+				curl_setopt($curly[$id], CURLOPT_VERBOSE, true);
+			}
 
-            // post?
-            if (is_array($d)) {
-                if (!empty($d['post'])) {
-                    curl_setopt($curly[$id], CURLOPT_POST, true);
-                    curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['post']);
-                }
-            }
+			if ($this->isPost) {
+				curl_setopt($curly[$id], CURLOPT_POST, true);
+			}
 
-			// json?
-			 else if (!empty($d['payload'])) {
-				//attach encoded JSON string to the POST fields
-				curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['payload']);
-				//set the content type to application/json
-				curl_setopt($curly[$id], CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+			// post?
+			if (is_array($d)) {
+
+				if (!empty($d['post'])) {
+
+					curl_setopt($curly[$id], CURLOPT_POST, true);
+					curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['post']);
+
+				}
+
+				// json?
+				if (!empty($d['payload'])) {
+
+					//attach encoded JSON string to the POST fields
+					curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['payload']);
+					//set the content type to application/json
+					curl_setopt($curly[$id], CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+				}
 
 			}
 
-            // extra options?
-            if (!empty($options)) {
-                curl_setopt_array($curly[$id], $options);
-            }
+			// extra options?
+			if (!empty($options)) {
+				curl_setopt_array($curly[$id], $options);
+			}
 
-            curl_multi_add_handle($mh, $curly[$id]);
-        }
+			curl_multi_add_handle($mh, $curly[$id]);
+		}
 
-        // execute the handles
-        $running = null;
-        do {
-            curl_multi_exec($mh, $running);
-        } while ($running > 0);
+		// execute the handles
+		$running = null;
+		do {
+			curl_multi_exec($mh, $running);
+		} while ($running > 0);
 
-        // get content and remove handles
-        foreach ($curly as $id => $c) {
-            if ($this->isJson) {
-                json_decode(curl_multi_getcontent($c));
-                if (json_last_error() === JSON_ERROR_NONE) {
-                    $result[$id] = json_decode(curl_multi_getcontent($c), true);
-                } else {
-                    $result[$id] = curl_multi_getcontent($c);
-                }
-            } else {
-                $result[$id] = curl_multi_getcontent($c);
-            }
-            curl_multi_remove_handle($mh, $c);
-        }
-        // all done
-        curl_multi_close($mh);
+		// get content and remove handles
+		foreach ($curly as $id => $c) {
 
-        return $result;
-    }
+			if ($this->isJson) {
 
-    /**
-     * @param bool $isJson
-     */
-    public function setIsJson($isJson)
-    {
-        $this->isJson = $isJson;
-    }
+				json_decode(curl_multi_getcontent($c));
+				if (json_last_error() === JSON_ERROR_NONE) {
+					$result[$id] = json_decode(curl_multi_getcontent($c), true);
+				} else {
+					$result[$id] = curl_multi_getcontent($c);
+				}
 
-    /**
-     * @return bool
-     */
-    public function isJson()
-    {
-        return $this->isJson;
-    }
+			} else {
+				$result[$id] = curl_multi_getcontent($c);
+			}
+			curl_multi_remove_handle($mh, $c);
 
-    /**
-     * @return bool
-     */
-    public function isDebug()
-    {
-        return $this->isDebug;
-    }
+		}
+		// all done
+		curl_multi_close($mh);
 
-    /**
-     * @param bool $isDebug
-     */
-    public function setIsDebug($isDebug)
-    {
-        $this->isDebug = $isDebug;
-    }
+		return $result;
+	}
+
+	/**
+	 * @param bool $isJson
+	 */
+	public function setIsJson($isJson)
+	{
+
+		$this->isJson = $isJson;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isJson()
+	{
+
+		return $this->isJson;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isDebug()
+	{
+
+		return $this->isDebug;
+	}
+
+	/**
+	 * @param bool $isDebug
+	 */
+	public function setIsDebug($isDebug)
+	{
+
+		$this->isDebug = $isDebug;
+	}
 
 	/**
 	 * @return bool
 	 */
 	public function isPost()
 	{
+
 		return $this->isPost;
 	}
 
@@ -156,6 +172,7 @@ class MultiProccessingCurlRequests
 	 */
 	public function setIsPost($isPost)
 	{
+
 		$this->isPost = $isPost;
 	}
 
